7c6e49db85eb6cffb2634428a8b660eac8530838
Pedro Diniz <dropedz@protonmail.com>
2018-10-13 02:02:15 +0100

    Commit my changes
    * Update the license for compatibility (the original work's license is CC-BY-SA-2.0)  
    * Added a way to increase/decrease speed  
    * Add comments to some parts of the code  
    * Remade the player image in png  
    * The background on the menu now rotates  
    * The cube colors are now always randomly changing  
    * Changed the way the score is calculated to account for speed  
    * Made it possible to change the speed without leveling up

--- CubeField.lua
+++ CubeField.lua
@@ -1,20 +1,19 @@
-playerImg=image.new("\020\000\000\000\020\000\000\000\000\000\000\000(\000\000\000\016\000\001\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\000\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\128\000\128\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\000\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\128\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\128\000\128\000\128")
+playerImg=image.new(_R.IMG.img_1)
 
 cubesX={1,5,7,6,4,6,4,1,3,2,3,7,8,9,1,2,3,4,2,6,7,1,9,1,2,3,4}
 cubesY={1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9}
 lastCube=1
 player=0
-speed=0.08
+speed=0.08                                                --default speed
 moving=false
 dir=1
-moveStart=0
+moveStart=0                                               --default position
 menu=true
-score=0
-level=1
-color={225,170,0}
-rotation=0
+score=0                                                   --preset score
+level=1                                                   --preset level
+rotation=50                                               --this makes the screen continuously rotate for some reason
 lastTick=0
-delay=0
+delay=0                                                   --default timer time(delay)
 
 function render(gc)
  drawHorizon(gc)
@@ -25,8 +24,8 @@ function render(gc)
   x=(cubesX[i]-7-player)/(10-cubesY[i])*150-size/2
   y=5/(10-cubesY[i])*50-180
   polygon=rotate({x,y,x+size,y,x+size,y+size,x,y+size,x,y})
-  if cubesY[i]>1 and y+size<platform.window.height() then -- Critor: fillPolygon and drawPolyLine return "value out of range" if y is too high, unlike drawLine
-   gc:setColorRGB(color[1],color[2],color[3])
+  if cubesY[i]>1 and y+size<platform.window.height() then              --Critor: fillPolygon and drawPolyLine return "value out of range" if y is too high, unlike drawLine
+   gc:setColorRGB(math.random(255),math.random(255),math.random(255))  --set cube color to random
    gc:fillPolygon(polygon)
    gc:setColorRGB(0,0,0)
    gc:drawPolyLine(polygon)
@@ -102,7 +101,7 @@ function drawMenu(gc)
    highscore=0
   end
   if score>highscore then
-   var.store("highscore",score)
+   var.store("highscore",score)                           --store the highscore if the current score is higher than the previously stored highscore
    document.markChanged()
   end
   local str="Score : "..tostring(score).."              Highscore : "..tostring(highscore)
@@ -110,32 +109,24 @@ function drawMenu(gc)
  end
 end
 
+--Drawing score
 function drawScore(gc)
- if not menu then
-  gc:setColorRGB(0,0,0)
-  gc:setFont("sansserif","r",12)
-  gc:drawString(tostring(math.floor(score)),2,0,"top")
-  if score%1000>950 then
+ if not menu then                                         --draw score when game is playing
+  gc:setColorRGB(0,0,0)                                   --set score letter color black
+  gc:setFont("sansserif","r",12)                          --set letter type sansserif, regular, size 12
+  gc:drawString(tostring(math.floor(score)),2,0,"top")    --draw score on the top left corner
+  if score%1000>950 then                                  --draw "Speed Up !" when the score is between 950 and 1000
    gc:setFont("sansserif","r",30)
    gc:drawString("Speed Up !",80,10,"top")
   end
  end
 end
 
+--Leveling up
 function levelUp()
- speed=speed+0.02
- level=level+1
- if (level-1)%5==0 then
-  color={225,170,0}
- elseif (level-1)%5==1 then
-  color={0,0,255}
- elseif (level-1)%5==2 then
-  color={100,100,100}
- elseif (level-1)%5==3 then
-  color={90,255,0}
- elseif (level-1)%5==4 then
-  color={255,255,255}
- end
+ speed=speed+0.01                                         --increase speed by 0.01
+ level=level+1                                            --increase level by 1
+ score=score+50                                           --increase score by 50
 end
 
 function on.paint(gc)
@@ -145,25 +136,24 @@ function on.paint(gc)
  drawScore(gc)
  gc:setColorRGB(0,0,0)
  gc:setFont("sansserif","r",8)
- gc:drawString("Lua CubeField - Par Loic Pujet",10,200,"top")
  updateFPS()
  timer.start(0.01)
 end
 
-acc = 0 -- Critor's accelerometer
+acc = 0                                                   --Critor's accelerometer
 
 function on.timer()
- local tacc = math.eval("getacc()")     -- Critor's accelerometer
- if tacc~=nil then                      -- Critor's accelerometer
-  acc = tacc                            -- Critor's accelerometer
- end                                    -- Critor's accelerometer
- if acc<-2 then on.arrowLeft() end      -- Critor's accelerometer
- if acc>2 then on.arrowRight() end      -- Critor's accelerometer
+ local tacc = math.eval("getacc()")                       --Critor's accelerometer
+ if tacc~=nil then                                        --Critor's accelerometer
+  acc = tacc                                              --Critor's accelerometer
+ end                                                      --Critor's accelerometer
+ if acc<-2 then on.arrowLeft() end                        --Critor's accelerometer
+ if acc>2 then on.arrowRight() end                        --Critor's accelerometer
  timer.stop()
  updateCubes()
  updateMove()
  if not menu then
-  score=score+delay/30
+  score=score+(math.sqrt(delay))*2*speed                  --update score by adding the preexisting score to the square root of the delay times 2 times the speed
   if score>level*1000 then
    levelUp()
   end
@@ -171,33 +161,54 @@ function on.timer()
  platform.window:invalidate()
 end
 
+--Game start (pressing enter at the main menu)
 function on.enterKey()
  if menu then
-  menu=false
-  speed=0.08
+  menu=false                                              --leave menu
+  speed=0.08                                              --set speed at 0.08
   moving=false
-  score=0
-  level=1
-  color={225,170,0}
+  rotation=0                                              --set rotation at 0 to prevent the screen from continuously rotating
+  score=0                                                 --start with score 0
+  level=1                                                 --set level at 1
   for i=1,27 do
    cubesY[i]=cubesY[i]-5
   end
  end
 end
 
+--Turning to the left
 function on.arrowLeft()
  if not menu then
-  dir=-1
+  dir=-1                                                  --go to the left by 1
   moving=true
   moveStart=timer.getMilliSecCounter()
  end
 end
 
+--Turning to the right
 function on.arrowRight()
  if not menu then
-  dir=1
+  dir=1                                                   --go to the right by 1
   moving=true
   moveStart=timer.getMilliSecCounter()
+ end
+end
+
+--Increasing speed
+function on.arrowUp()
+ if not menu then
+  speed=speed+0.01                                        --increase speed by 0.01
+ end
+end
+
+--Decreasing speed
+function on.arrowDown()
+ if not menu then
+  if speed<0.08+(level*0.01) then
+   speed=speed                                            --maintain speed to prevent going backwards
+  else
+   speed=speed-0.01                                       --decrease speed by 0.01
+  end
  end
 end
 
@@ -206,6 +217,10 @@ function on.charIn(ch)
   on.arrowRight()
  elseif ch=="4" then
   on.arrowLeft()
+ elseif ch=="8" then
+  on.arrowUp()
+ elseif ch=="5" then
+  on.arrowDown()
  end
 end
 
